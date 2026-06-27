4fcf7155a9511b7de382728fc46d4885cfe4bb10
gillbert-joshua77 <gillbertjoshua77@gmail.com>
2026-06-27 13:30:08 +0530

    Problem 1295 : Find Numbers with Even Number of Digits

--- src/FindNumberswithEvenNumberofDigits/FindNumberswithEvenNumberofDigits.java
+++ src/FindNumberswithEvenNumberofDigits/FindNumberswithEvenNumberofDigits.java
@@ -1,4 +1,16 @@
 package FindNumberswithEvenNumberofDigits;
 
-public class FindNumberswithEvenNumberofDigits {
+class Solution {
+    public int findNumbers(int[] nums) {
+        int cnt=0;
+        for(int num:nums){
+            if(((int)Math.log10(num)+1)%2==0) cnt++;
+        }
+        return cnt;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int [] nums = {12,345,2,6,7896};
+        System.out.println(s.findNumbers(nums));
+    }
 }
