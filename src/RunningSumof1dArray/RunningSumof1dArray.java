ab6123af2e178c7e8a61460d504d1d4a373dbaf8
gillbert-joshua77 <gillbertjoshua77@gmail.com>
2026-06-27 13:36:57 +0530

    Problem 1480 : Running Sum of 1d Array

--- src/RunningSumof1dArray/RunningSumof1dArray.java
+++ src/RunningSumof1dArray/RunningSumof1dArray.java
@@ -1,4 +1,25 @@
 package RunningSumof1dArray;
 
-public class RunningSumof1dArray {
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int [] ans  = new int[nums.length];
+        for (int i = 0 ; i < nums.length ; i++){
+            if(i == 0){
+                ans[i]=nums[i];
+            }else{
+                ans[i] = ans[i-1] + nums[i];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums  = {1,2,3,4};
+
+        int[] result =  s.runningSum(nums);
+        for(int i:result){
+            System.out.print(i+" ");
+        }
+
+    }
 }
